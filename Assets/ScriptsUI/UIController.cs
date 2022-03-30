6e6237618e993dba50cabf4312b2db9f2b5e8a89
Jorge Medina <jorgemedinaleigh@protonmail.com>
2022-03-30 12:19:02 -0300

    Added background & platforms to the scene

---
+++ Assets/ScriptsUI/UIController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] GameObject endGamePanel;
+    [SerializeField] float totalTime;
+    [SerializeField] TextMeshProUGUI timer;
+
+    void Awake()
+    {
+        endGamePanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        totalTime -= Time.deltaTime;
+
+        float minutes = Mathf.FloorToInt(totalTime / 60);
+        float seconds = Mathf.FloorToInt(totalTime % 60);
+        float milliseconds = (totalTime % 1) * 1000;
+
+        timer.text = "Time: " + string.Format("{0:00}:{1:00}:{2:0}", minutes, seconds, milliseconds);
+
+        //totalTime -= Time.deltaTime;
+        //timer.text = totalTime.ToString("F1");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("Se ha presionado el boton de Play Again");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Se ha presionado el boton de Quit Game");
+    }
+
+    public void PlayerDeath()
+    {
+        endGamePanel.SetActive(true);
+    }
+}
